5947516bdad194b129274064dbed83d1592c6886
Mathias Quintero <me@quintero.io>
2022-02-23 02:06:12 +0000

    updating sync

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SyncWebSocketWebAssemblyClient"]),
     ],
     dependencies: [
-        .package(name: "Sync", url: "https://github.com/nerdsupremacist/Sync.git", from: "1.0.0"),
+        .package(name: "Sync", url: "https://github.com/nerdsupremacist/Sync.git", from: "1.0.2"),
         .package(url: "https://github.com/swiftwasm/JavaScriptKit.git", .upToNextMinor(from: "0.12.0")),
     ],
     targets: [
