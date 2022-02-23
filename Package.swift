3a0e484deb122c2e08c33f5b61b4d06a47838510
Mathias Quintero <me@quintero.io>
2022-02-23 00:07:28 +0000

    Adding basic connection

--- Package.swift
+++ Package.swift
@@ -5,22 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "SyncWebSocketWebAssemblyClient",
+    platforms: [.macOS(.v11), .iOS(.v14), .watchOS(.v6), .tvOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SyncWebSocketWebAssemblyClient",
             targets: ["SyncWebSocketWebAssemblyClient"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Sync", url: "https://github.com/nerdsupremacist/Sync.git", from: "1.0.0"),
+        .package(url: "https://github.com/swiftwasm/JavaScriptKit.git", .upToNextMinor(from: "0.12.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SyncWebSocketWebAssemblyClient",
-            dependencies: []),
+            dependencies: [
+                "Sync",
+                .product(
+                    name: "JavaScriptKit",
+                    package: "JavaScriptKit",
+                    condition: .when(platforms: [.wasi])
+                ),
+            ]),
         .testTarget(
             name: "SyncWebSocketWebAssemblyClientTests",
             dependencies: ["SyncWebSocketWebAssemblyClient"]),
--- Sources/SyncWebSocketWebAssemblyClient/SyncWebSocketWebAssemblyClient.swift
+++
@@ -1,6 +0,0 @@
-public struct SyncWebSocketWebAssemblyClient {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
