538bb193f364deda3ded82f4924716d412c42247
Mathias Quintero <me@quintero.io>
2022-02-23 02:01:06 +0000

    Fixing data serialization for web sockets

--- Sources/SyncWebSocketWebAssemblyClient/WebSocketClientConnection.swift
+++ Sources/SyncWebSocketWebAssemblyClient/WebSocketClientConnection.swift
@@ -22,7 +22,6 @@ public class WebSocketClientConnection: ConsumerConnection {
     }
 
     private static let webSocketConstructor = JSObject.global.WebSocket.function!
-    private static let textEncoder = JSObject.global.TextDecoder.function!.new("utf-8")
 
     @Published
     public fileprivate(set) var isConnected: Bool = false
@@ -56,57 +55,12 @@ public class WebSocketClientConnection: ConsumerConnection {
 
             var webSocketObject: JSObject? = nil
             var onOpen: JSClosure?
-            onOpen = JSClosure { [weak self] _ in
-                guard let self = self else {
-                    return .undefined
-                }
-
-                if let onOpen = onOpen {
-                    _ = self.webSocketObject?.removeEventListener!("open", onOpen)
-                }
-
-                onOpen = nil
-                return .undefined
-            }
-
-            var onMessage: JSClosure?
-            onMessage = JSClosure { [weak self] arguments in
-                guard let self = self else { return .undefined }
-                let event = arguments.first!.object!
-                switch event["data"] {
-                case .string(let dataString):
-                    guard let data = String(dataString).data(using: .utf8) else { break }
-                    self.receivedDataSubject.send(data)
-                case .object(let object):
-                    let dataString = Self.textEncoder.decode!(object).string;
-                    guard let data = dataString?.data(using: .utf8) else { break }
-                    self.receivedDataSubject.send(data)
-                default:
-                    break
-                }
-                return .undefined
-            }
 
             var onFirstMessage: JSClosure? = nil
-            onFirstMessage = JSClosure { arguments in
-                let event = arguments.first!.object!
-                switch event["data"] {
-                case .string(let dataString):
-                    guard let data = String(dataString).data(using: .utf8) else { break }
-                    _ = webSocketObject?.removeEventListener!("message", onFirstMessage)
-                    onFirstMessage = nil
-                    _ = webSocketObject?.addEventListener!("message", onMessage)
-                    continuation.resume(returning: data)
-                    return .undefined
-                default:
-                    break
-                }
-                continuation.resume(throwing: WebSocketError.invalidMessageFromWebSocketOnFirstMessage)
-                return .undefined
-            }
-
+            var onMessage: JSClosure?
             var onClose: JSClosure?
-            onClose = JSClosure { [weak self] _ in
+
+            let close = { [weak self] in
                 if let listener = onFirstMessage {
                     _ = webSocketObject?.removeEventListener!("message", listener)
                     onFirstMessage = nil
@@ -119,6 +73,51 @@ public class WebSocketClientConnection: ConsumerConnection {
                 if let self = self {
                     self.isConnected = false
                 }
+            }
+
+            onOpen = JSClosure { [weak self] _ in
+                guard let self = self else {
+                    return .undefined
+                }
+
+                if let onOpen = onOpen {
+                    _ = self.webSocketObject?.removeEventListener!("open", onOpen)
+                }
+
+                onOpen = nil
+                self.isConnected = true
+                return .undefined
+            }
+
+            onMessage = JSClosure { [weak self] arguments in
+                guard let self = self else { return .undefined }
+                let event = arguments.first!.object!
+                event.dataFromMessageEvent { [weak self] data in
+                    guard let data = data else { return }
+                    self?.receivedDataSubject.send(data)
+                }
+                return .undefined
+            }
+
+            onFirstMessage = JSClosure { arguments in
+                let event = arguments.first!.object!
+                event.dataFromMessageEvent { data in
+                    guard let data = data else {
+                        continuation.resume(throwing: WebSocketError.invalidMessageFromWebSocketOnFirstMessage)
+                        close()
+                        return
+                    }
+                    self.receivedDataSubject.send(data)
+                    _ = webSocketObject?.removeEventListener!("message", onFirstMessage)
+                    onFirstMessage = nil
+                    _ = webSocketObject?.addEventListener!("message", onMessage)
+                    continuation.resume(returning: data)
+                }
+                return .undefined
+            }
+
+            onClose = JSClosure { _ in
+                close()
                 return .undefined
             }
 
@@ -147,6 +146,58 @@ public class WebSocketClientConnection: ConsumerConnection {
     public func receive() -> AnyPublisher<Data, Never> {
         return receivedDataSubject.eraseToAnyPublisher()
     }
+}
+
+extension JSObject {
+    private static let blob = JSObject.global.Blob.function!
+    private static let fileReader = JSObject.global.FileReader.function!
+    private static let textEncoder = JSObject.global.TextDecoder.function!.new("utf-8")
+
+    func dataFromMessageEvent(
+        completion: @escaping (Data?) -> Void
+    ) {
+        switch self["data"] {
+        case .string(let dataString):
+            completion(String(dataString).data(using: .utf8))
+        case .object(let object) where object.isInstanceOf(Self.blob):
+            var onLoaded: JSClosure?
+            var onError: JSClosure?
+            let reader = Self.fileReader.new(object)
+            onLoaded = JSClosure { arguments in
+                print(arguments)
+                onLoaded = nil
+                onError = nil
+
+                guard let base64String = reader.result.string?.replacingOccurrences(of: "^data:.+;base64,", with: "", options: .regularExpression) else {
+                    completion(nil)
+                    return .undefined
+                }
+
+                print(base64String)
+                completion(Data(base64Encoded: base64String))
+                return .undefined
+            }
+
+            onError = JSClosure { error in
+                print(error)
+                onLoaded = nil
+                onError = nil
+                completion(nil)
+                return .undefined
+            }
+
+            reader.onloadend = .object(onLoaded!)
+            reader.onerror = .object(onError!)
+            reader.onabort = .object(onError!)
+            _ = reader.readAsDataURL!(object)
+        case .object(let object):
+            let dataString = Self.textEncoder.decode!(object).string
+            completion(dataString?.data(using: .utf8))
+        default:
+            completion(nil)
+        }
+    }
+
 }
 
 #endif
